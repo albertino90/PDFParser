ec38ad51cd7bcd387d4b9e32d2f2fdd3006fb0ff
Albert N <unilon90@mail.ru>
2019-10-28 14:12:33 +0500

    automatic email download added

--- src/main/java/EmailParser.java
+++ src/main/java/EmailParser.java
@@ -4,6 +4,7 @@ import javax.mail.internet.MimeUtility;
 import javax.mail.search.FlagTerm;
 import java.io.*;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.Properties;
 
@@ -21,55 +22,61 @@ public class EmailParser {
         try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
              Store store = session.getStore()) {
             store.connect(host, login, password);
-            //получаем папку с входящими сообщениями
             Folder inbox = store.getFolder("INBOX");
-            //открываем её только для чтения
-            inbox.open(Folder.READ_ONLY);
-            Message messages[] = inbox.search(new FlagTerm(new Flags(
+            inbox.open(Folder.READ_WRITE);
+
+            Message[] messages = inbox.search(new FlagTerm(new Flags(
                     Flags.Flag.SEEN), false));
-//                for (int i = 012119; i < messages.length; i++) {
-//                    System.out.println(messages[i].getMessageNumber());
-//                }
-            System.out.println("Общее количество сообщений : " + inbox.getMessageCount() + "\n"
-                    + "Количество непрочитанных сообщений : " + messages.length + "\n");
-            if (inbox.getMessageCount() == 0) {
-                System.out.println("Сообщений нет");
+
+            if (messages.length == 0){
+                System.out.println("Необработанных сообщений нет");
             }
-            System.out.println("Введите номер письма");
-            int postNumber = Integer.parseInt(bufferedReader.readLine());
-            //получаем последнее сообщение (самое старое будет под номером 1)
-            Message message = inbox.getMessage(postNumber);
-            //Return the content as a Java object
-            Multipart mp = (Multipart) message.getContent();
-            //Return the number of enclosed BodyPart objects
-            int numberOfParts = mp.getCount();
+            for (int i = 1, j = messages.length; i <= j ; j--) {
+                Message message = inbox.getMessage(j);
+                //Return the content as a Java object
+                Multipart mp = (Multipart) message.getContent();
+                //Return the number of enclosed BodyPart objects
+                int numberOfParts = mp.getCount();
 //                System.out.println("number of parts "+numberOfParts);
-            for (int partCount = 0; partCount < numberOfParts; partCount++) {
-                MimeBodyPart part = (MimeBodyPart) mp.getBodyPart(partCount);
-                if (Part.ATTACHMENT.equalsIgnoreCase(part.getDisposition())) {
-                    //дата получения
-                    Date messageDate = message.getReceivedDate();
-                    SimpleDateFormat strFormat = new SimpleDateFormat("MM.dd_HH.mm.ss");
-                    String receiptTime = strFormat.format(messageDate);
-                    String filePath = "C:/Dir/оригPDF/" + receiptTime + ".pdf";
-                    //сохранение PDf на диск
-                    part.saveFile(filePath);
-                    //адресс отчета
-                    String reportFilepath = "C:/Dir/Отчет/" + receiptTime + ".txt";
-                    //парсим
-                    PdfParser parser = new PdfParser();
-                    parser.setFileName(filePath);
-                    parser.setFileNameDest(reportFilepath);
-                    parser.parsePDF();
-                    System.out.println("Отчет сформирован");
+                for (int partCount = 0; partCount < numberOfParts; partCount++) {
+                    MimeBodyPart part = (MimeBodyPart) mp.getBodyPart(partCount);
+                    if (Part.ATTACHMENT.equalsIgnoreCase(part.getDisposition())) {
+                        //дата получения
+                        Date messageDate = message.getReceivedDate();
+                        SimpleDateFormat strFormat = new SimpleDateFormat("MM.dd_HH.mm.ss");
+                        String receiptTime = strFormat.format(messageDate);
+                        String filePath = "C:/Dir/оригPDF/" + receiptTime + ".pdf";
+                        //сохранение PDf на диск
+                        part.saveFile(filePath);
+                        //адресс отчета
+                        String reportFilepath = "C:/Dir/Отчет/" + receiptTime + ".txt";
+                        //парсим
+                        PdfParser parser = new PdfParser();
+                        parser.setFileName(filePath);
+                        parser.setFileNameDest(reportFilepath);
+                        parser.parsePDF();
+                        System.out.println("Отчет по письму "+ receiptTime +" сформирован");
+                    }
                 }
             }
+//            System.out.println("Общее количество сообщений : " + inbox.getMessageCount() + "\n"
+//                    + "Количество непрочитанных сообщений : " + messages.length + "\n");
+//            if (inbox.getMessageCount() == 0) {
+//                System.out.println("Сообщений нет");
+//            }
+//            System.out.println("Введите номер письма");
+//            int postNumber = Integer.parseInt(bufferedReader.readLine());
+            //получаем последнее сообщение (самое старое будет под номером 1)
+
         } catch (NoSuchProviderException e) {
             System.err.println(e.getMessage());
+            e.printStackTrace();
         } catch (MessagingException e) {
             System.err.println(e.getMessage());
+            e.printStackTrace();
         } catch (IOException e) {
             System.err.println(e.getMessage());
+            e.printStackTrace();
         }
     }
 
